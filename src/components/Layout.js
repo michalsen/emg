ee524a86d7601b203df620a49f55fe402a13aabc
Eric Michalsen <emichalsen@straightnorth.com>
2021-02-10 13:52:47 -0500

    mobile polygon original

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -28,7 +28,7 @@ const ContentStyles = styled.div`
   padding: 4rem;
   /* https://bennettfeely.com/clippy/ */
   @media (max-width: 800px) {
-    clip-path: polygon(0  0, 0 120%, 200% 0);
+    clip-path: polygon(0  0, 0 50%, 150% 0);
   }
   @media (min-width: 801px) {
     clip-path: polygon(0 0, 0 100%, 115% 0);
@@ -43,7 +43,7 @@ export default function Layout({ children }) {
       <Typography />
       {/* <SiteBorderStyles> */}
         <ContentStyles>
-          <div class="sorry center">currently working on mobile version ... sorry.</div>
+          <div class="sorry center">currently working on mobile version... sorry.</div>
           <Nav />
           {children}
           {/* <Footer /> */}
