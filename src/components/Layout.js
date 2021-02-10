e55422b0b52715d68ecee4d078a1df35b0db25a0
Eric Michalsen <emichalsen@straightnorth.com>
2021-02-10 10:00:56 -0500

    query < 800 set-clip

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -28,7 +28,7 @@ const ContentStyles = styled.div`
   padding: 4rem;
   /* https://bennettfeely.com/clippy/ */
   @media (max-width: 800px) {
-    clip-path: polygon(0  0, 0 110%, 150% 0);
+    clip-path: polygon(0  0, 0 100%, 150% 0);
   }
   @media (min-width: 801px) {
     clip-path: polygon(0 0, 0 100%, 100% 0);
